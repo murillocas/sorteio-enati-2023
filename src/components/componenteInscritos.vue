51c70cbb6fd32d4211b4f6e2cc93cdd791b2639c
Murillo Santos de Castro <murillocastro2009@hotmail.com>
2023-05-14 20:11:40 -0300

    teste sorteio

--- src/components/componenteInscritos.vue
+++ src/components/componenteInscritos.vue
@@ -20,7 +20,7 @@
 
 <script>
 import axios from "axios";
-import { API_URL_INSCRITOS } from "@/api";
+import { API_URL_INSCRITOS, API_TOKEN } from "@/api";
 
 export default {
   name: "componenteInscritos",
@@ -33,66 +33,107 @@ export default {
       NomeSorteado: "Nome do Sorteado",
       ListaAuxSorteio: [],
       QTDJASON:null,
+      listaAtual:1,
     };
   },
   created() {
     this.obterListaInscritos();
+    this.preencherListaAux();
   },
   methods: {
     obterListaInscritos() {
-      axios.get(API_URL_INSCRITOS).then((resposta) => {
+     
+        axios.get(API_URL_INSCRITOS ).then((resposta) => {
+          console.log(resposta.data);
+          this.Linscritos = resposta.data.results;
+          console.log(this.Linscritos);
+          //this.next = resposta.data.next;
+          this.prev = resposta.data.previous;
+          this.QTDINscrito = resposta.data.count;
+          this.QTDJASON = this.Linscritos.length;
+          
+           this.preencherListaAux();
+
+        })
+        .catch((erro) => {
+          console.log(erro);
+        });
+        console.log(this.ListaAuxSorteio);
+    }, obterListaInscritosteste() {
+      var pegarProximo = true;
+      var pagina = 1;
+      while(pegarProximo){
+        axios.get(API_URL_INSCRITOS +"?page=" + pagina,{headers: {'Authorization': API_TOKEN}}).then((resposta) => {
           console.log(resposta.data);
           this.Linscritos = resposta.data.results;
           console.log(this.Linscritos);
           this.next = resposta.data.next;
           this.prev = resposta.data.previous;
           this.QTDINscrito = resposta.data.count;
-          this.QTDJASON = this.Linscritos.length;
+          //this.QTDJASON = this.Linscritos.length;
 
-        for(var x =0;x< this.QTDINscrito; x++){
-            this.ListaAuxSorteio.push(x);
-        }
         })
         .catch((erro) => {
           console.log(erro);
         });
 
+        if(this.next == null){
+          pegarProximo = false;
+          pagina +=1;
+        }
+
+      }
+     
        
         console.log(this.ListaAuxSorteio);
     },
     sortear() {
        if(this.ListaAuxSorteio.length == 0){
             this.preencherListaAux();
-      }
-      var NomedoSorteado;
+        }
+      //var NomedoSorteado;
 
+      //sortear não repetidos com base na lista auxiliar
       var QtdNaListaAux = this.ListaAuxSorteio.length;
       var NumeroSorteado = Math.floor(Math.random() *  QtdNaListaAux)
-      var qtdlista = Math.ceil( this.QTDINscrito/this.QTDJASON);
-      var  PosiListaSorteada =Math.ceil( NumeroSorteado/this.QTDJASON);
-      var posiSorteada = NumeroSorteado % this.QTDJASON;
+
+      //pegar o valor na posição sorteada na lista auxiliar
+      if(this.NumeroSorteado == 0){
+          //remove o primeiro elemento
+          NumeroSorteado = this.ListaAuxSorteio.shift();
+      }else{
+        // remove o elemento na posiSorteada
+         NumeroSorteado = this.ListaAuxSorteio.splice(NumeroSorteado,1);
+      }
+
+      // identificar a qtd de listas possiveis 
+      //var qtdlista = Math.ceil( this.QTDINscrito/this.QTDJASON);
+
+      // identificar em qual lista esta o numero sorteado
+     //var  PosiListaSorteada =Math.ceil( NumeroSorteado/this.QTDJASON);
+      //if(PosiListaSorteada == 0){
+       // PosiListaSorteada = 1
+      //}
+
+      // identificar em qual possição da lista esta o numero
+      //var posiSorteada = NumeroSorteado % this.QTDJASON;
 
 
       console.log("qtd é : " +  QtdNaListaAux);
       console.log("sorteado"+NumeroSorteado);
-      console.log("qtd lista"+qtdlista);
-      console.log("em qual lista esta" + PosiListaSorteada);
-      console.log("posi sorteada"+posiSorteada);
+     // console.log("qtd lista"+qtdlista);
+      //console.log("em qual lista esta" + PosiListaSorteada);
+      //console.log("posi sorteada"+posiSorteada);
+
+      // atualiza o jzon coso esteja em outra pagina
       
-
-
-
-        
-      /*if(this.rondom == 0){
-            sorter NomedoSorteado = this.ListaAuxSorteio.shift();
-      }else{
-         sorter NomedoSorteado = this.ListaAuxSorteio.splice(rondom,1);
-      }*/
-
+      //if(this.listaAtual != PosiListaSorteada){
+       // this.atualizarJzon(PosiListaSorteada);
+      //}
+      
+      
       //console.log("o numero sorteado e :" + sorteado + " rondom : " + rondom)
-
-
-      this.NomeSorteado = "" + NomedoSorteado;
+      this.NomeSorteado = this.Linscritos[NumeroSorteado].nome;
     },
 
 
@@ -100,10 +141,25 @@ export default {
         for(var x =0;x< this.QTDINscrito; x++){
             this.ListaAuxSorteio.push(x);
         }
-    }
+    },
+    /*atualizarJzon(posiLista){
+         axios.get(API_URL_INSCRITOS).then((resposta) => {
+          console.log(resposta.data);
+          this.Linscritos = resposta.data.results;
+          console.log(this.Linscritos);
+          this.next = resposta.data.next;
+          this.prev = resposta.data.previous;
+          this.listaAtual = posiLista;
+        })
+        .catch((erro) => {
+          console.log(erro);
+        });
+    },*/
 
   },
+  
 };
+
 </script>
 
 
